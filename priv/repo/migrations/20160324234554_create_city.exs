bc9667f992d0a69f962df0869fa036c3c6fe9f55
Mirosław Ciastek <miroslaw.ciastek@elpassion.com>
2016-03-25 00:50:56 +0100

    Added City model.

---
+++ priv/repo/migrations/20160324234554_create_city.exs
@@ -0,0 +1,11 @@
+defmodule EmotionsWheelBackend.Repo.Migrations.CreateCity do
+  use Ecto.Migration
+
+  def change do
+    create table(:cities) do
+      add :name, :string
+      timestamps
+    end
+
+  end
+end
---
+++ test/models/city_test.exs
@@ -0,0 +1,18 @@
+defmodule EmotionsWheelBackend.CityTest do
+  use EmotionsWheelBackend.ModelCase
+
+  alias EmotionsWheelBackend.City
+
+  @valid_attrs %{}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = City.changeset(%City{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = City.changeset(%City{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
