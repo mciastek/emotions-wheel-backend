910fb82b40186c5d9e77bd4c0f7d5f6300093fd1
Mirosław Ciastek <miroslaw.ciastek@elpassion.com>
2016-03-26 00:00:45 +0100

    Added seeds for researcher.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,18 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias EmotionsWheelBackend.{Researcher, Repo}
+
+# Researchers
+Repo.insert!(
+  Researcher.changeset(
+    %Researcher{}, %{
+    email: "test@mail.com",
+    password: "password",
+    password_confirmation: "password",
+    first_name: "John",
+    last_name: "Doe",
+    phone: "123123123"
+  })
+)
