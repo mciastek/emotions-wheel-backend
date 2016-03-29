b58bc0feea1845db1fa9bf87166ce76bbec6366f
Mirosław Ciastek <miroslaw.ciastek@elpassion.com>
2016-03-29 23:53:34 +0200

    Changed router to accept React routing

--- web/router.ex
+++ web/router.ex
@@ -16,7 +16,7 @@ defmodule EmotionsWheelBackend.Router do
   scope "/", EmotionsWheelBackend do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "*path", PageController, :index
   end
 
   # Other scopes may use custom stacks.
