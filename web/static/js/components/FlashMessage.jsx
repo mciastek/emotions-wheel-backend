cc3dc4cb2ef529221aeb7c4aeb93a2d9d1fd99de
Mirosław Ciastek <miroslaw.ciastek@elpassion.com>
2016-04-03 17:34:59 +0200

    Added flash-message component.

---
+++ web/static/js/components/FlashMessage.jsx
@@ -0,0 +1,28 @@
+import 'css/components/flash-message.scss';
+
+import React, { PropTypes } from 'react';
+import classNames from 'classnames';
+
+class FlashMessage extends React.Component {
+  render() {
+
+    const className = classNames({
+      'flash-message--success': this.props.type === 'success',
+      'flash-message--error': this.props.type === 'error',
+      'flash-message--warning': this.props.type === 'warning'
+    });
+
+    return (
+      <div className={className}>
+        <p className="flash-message__content">{this.props.message}</p>
+      </div>
+    );
+  }
+}
+
+FlashMessage.propTypes = {
+  type: PropTypes.string.isRequired,
+  message: PropTypes.string
+};
+
+export default FlashMessage;
