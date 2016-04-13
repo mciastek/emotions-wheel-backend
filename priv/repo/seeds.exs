4817fdacaabc391645606657e8eeba40c00bd8a9
Mirosław Ciastek <miroslaw.ciastek@elpassion.com>
2016-04-13 09:32:51 +0200

    Seeds change. Added assocciations build.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,45 +10,104 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-alias EmotionsWheelBackend.{Researcher, Participant, Country, Repo}
-
-# Countries
-Country.changeset(
-  %Country{}, %{
-    name: "Poland"
-  }
-) |> Repo.insert!
+alias EmotionsWheelBackend.{Researcher, Participant, Country, City, Language, Repo}
 
 # Researchers
-Researcher.changeset(
-  %Researcher{}, %{
+researchers = [
+  %Researcher{
     email: "test@mail.com",
     password: "password",
     password_confirmation: "password",
     first_name: "John",
     last_name: "Doe",
     phone: "123123123"
-}) |> Repo.insert!
+  }
+]
 
 # Participants
-Participant.changeset(
-  %Participant{}, %{
+{:ok, birthdate_participant_1} = Ecto.DateTime.load({{1941, 11, 30}, {0,0,0}})
+{:ok, birthdate_participant_2} = Ecto.DateTime.load({{1945, 10, 05}, {0,0,0}})
+
+participants = [
+  %Participant{
     email: "participant@test.com",
-    first_name: "Will",
-    last_name: "Smith",
-    birthdate: {{1941, 11, 30}, {0,0,0}},
+    first_name: "Jerzy",
+    last_name: "Stuhr",
+    birthdate: birthdate_participant_1,
     age: 75,
     gender: "male"
-  }
-) |> Repo.insert!
-
-Participant.changeset(
-  %Participant{}, %{
+  },
+  %Participant{
     email: "participant2@test.com",
     first_name: "Jane",
     last_name: "Fonda",
-    birthdate: {{1945, 01, 03}, {0,0,0}},
+    birthdate: birthdate_participant_2,
     age: 71,
     gender: "female"
   }
-) |> Repo.insert!
+]
+
+# Countries
+countries = [
+  %Country{
+    name: "Poland"
+  },
+  %Country{
+    name: "England"
+  }
+]
+
+# Languages
+languages = [
+  %Language{
+    name: "Polish",
+    code: "pl"
+  },
+  %Language{
+    name: "English",
+    code: "en"
+  }
+]
+
+# Cities
+cities = [
+  %City{
+    name: "Warsaw"
+  },
+  %City{
+    name: "London"
+  }
+]
+
+# Insert all repos
+researchers |> Enum.each(&Repo.insert!(&1))
+participants |> Enum.each(&Repo.insert!(&1))
+languages |> Enum.each(&Repo.insert!(&1))
+countries |> Enum.each(&Repo.insert!(&1))
+cities |> Enum.each(&Repo.insert!(&1))
+
+# Get certain records
+participant_1 = Repo.get_by(Participant, email: "participant@test.com")
+participant_2 = Repo.get_by(Participant, email: "participant2@test.com")
+
+poland = Repo.get_by(Country, name: "Poland")
+polish = Repo.get_by(Language, name: "Polish")
+warsaw = Repo.get_by(City, name: "Warsaw")
+
+england = Repo.get_by(Country, name: "England")
+english = Repo.get_by(Language, name: "English")
+london = Repo.get_by(City, name: "London")
+
+# Set assocciations
+Ecto.Changeset.change(warsaw, country_id: poland.id)
+|> Repo.update!
+
+Ecto.Changeset.change(london, country_id: england.id)
+|> Repo.update!
+
+Ecto.Changeset.change(participant_1, country_id: poland.id, city_id: warsaw.id, language_id: polish.id)
+|> Repo.update!
+
+Ecto.Changeset.change(participant_2, country_id: england.id, city_id: london.id, language_id: english.id)
+|> Repo.update!
+
