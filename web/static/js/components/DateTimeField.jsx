f0072985e6f085b18d073945373ae2f0fac3c53c
Mirosław Ciastek <miroslaw.ciastek@elpassion.com>
2016-04-27 21:15:09 +0200

    Added handle date and time format for DateTimeField

--- web/static/js/components/DateTimeField.jsx
+++ web/static/js/components/DateTimeField.jsx
@@ -1,19 +1,66 @@
 import 'css/components/datetime-field.scss';
 
 import React from 'react';
+import moment from 'moment';
 
 import InputDate from 'components/InputDate';
 import InputTime from 'components/InputTime';
 
 class DateTimeField extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      time: moment().format('H:mm:ss'),
+      date: moment().format('YYYY-MM-DD')
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!nextProps) return;
+
+    const { value } = nextProps;
+    const date = moment(nextProps.value).format('YYYY-MM-DD');
+    const time = moment(nextProps.value).format('H:mm:ss');
+
+    this.setState({
+      date,
+      time,
+      value
+    });
+  }
+
+  updateTime(_, value) {
+    const time = moment(value).format('H:mm:ss');
+
+    this._updateValue({ time });
+  }
+
+  updateDate(_, value) {
+    const date = moment(value).format('YYYY-MM-DD');
+
+    this._updateValue({ date });
+  }
+
+  _updateValue(value) {
+    const { date, time } = {
+      ...this.state,
+      ...value
+    };
+
+    this.setState({
+      value: `${date} ${time}`
+    });
+  }
+
   render() {
     return (
       <div className="datetime-field">
         <div className="datetime-field__col">
-          <InputTime hintText={this.props.timeLabel} />
+          <InputTime hintText={this.props.timeLabel} onChange={this.updateTime.bind(this)} />
         </div>
         <div className="datetime-field__col">
-          <InputDate hintText={this.props.dateLabel} />
+          <InputDate hintText={this.props.dateLabel} onChange={this.updateDate.bind(this)} />
         </div>
       </div>
     );
