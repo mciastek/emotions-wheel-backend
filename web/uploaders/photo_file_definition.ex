71f58daa08db7a473cc3f11c7fa2f764a41dd7d9
Mirosław Ciastek <miroslaw.ciastek@elpassion.com>
2016-05-28 22:34:54 +0200

    Added handle uploads to endpoint. Updated photo file definition.

--- web/uploaders/photo_file_definition.ex
+++ web/uploaders/photo_file_definition.ex
@@ -3,6 +3,7 @@ defmodule EmotionsWheelBackend.PhotoFileDefinition do
   use Arc.Ecto.Definition
 
   @versions [:original, :thumb]
+  @extension_whitelist ~w(.jpg .jpeg .gif .png)
 
   def transform(:thumb, _) do
     {:convert, "-strip -thumbnail 200x200^ -gravity center -extent 200x200 -format png", :png}
@@ -12,27 +13,14 @@ defmodule EmotionsWheelBackend.PhotoFileDefinition do
 
   def filename(version,  {file, _}), do: "#{version}-#{file.file_name}"
 
-  # To add a thumbnail version:
-  # @versions [:original, :thumb]
-
   # Whitelist file extensions:
-  # def validate({file, _}) do
-  #   ~w(.jpg .jpeg .gif .png) |> Enum.member?(Path.extname(file.file_name))
-  # end
-
-  # Define a thumbnail transformation:
-  # def transform(:thumb, _) do
-  #   {:convert, "-strip -thumbnail 250x250^ -gravity center -extent 250x250 -format png", :png}
-  # end
-
-  # Override the persisted filenames:
-  # def filename(version, _) do
-  #   version
-  # end
+  def validate({file, _}) do
+    @extension_whitelist |> Enum.member?(Path.extname(file.file_name))
+  end
 
   # Override the storage directory:
   # def storage_dir(version, {file, scope}) do
-  #   "uploads/user/avatars/#{scope.id}"
+  #   "uploads/photos/#{scope.id}"
   # end
 
   # Provide a default URL if there hasn't been a file uploaded
