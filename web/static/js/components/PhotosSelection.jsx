e81d9a2cd0d7cd57bb00a1d6fe27be769056845f
Mirosław Ciastek <miroslaw.ciastek@elpassion.com>
2016-06-05 18:31:13 +0200

    Added PhotosSelection component.

---
+++ web/static/js/components/PhotosSelection.jsx
@@ -0,0 +1,99 @@
+import 'css/components/photos-selection.scss';
+
+import React from 'react';
+import classNames from 'classnames';
+
+import { GridList, GridTile } from 'material-ui/lib/grid-list';
+import Checkbox from 'material-ui/lib/checkbox';
+
+import PhotoPreviewButton from 'containers/PhotoPreviewButton';
+
+const checkboxIconStyle = {
+  fill: 'white'
+}
+
+class PhotosSelection extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selection: [],
+      photos: []
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!nextProps) return;
+
+    const { selected = [] } = nextProps;
+
+    if (selected.length) {
+      this.setState({
+        selection: [...selected]
+      });
+    }
+  }
+
+  handleChange(photo, e, isChecked) {
+    if (isChecked) {
+      this.pushToSelection(photo[this.props.selectBy]);
+    } else {
+      this.removeFromSelection(photo[this.props.selectBy]);
+    }
+  }
+
+  pushToSelection(prop) {
+    this.setState({
+      selection: [
+        ...this.state.selection,
+        prop
+      ]
+    });
+  }
+
+  removeFromSelection(prop) {
+    this.setState({
+      selection: this.state.selection.filter((selected) => selected !== prop)
+    });
+  }
+
+  render() {
+    const thumbs = this.props.collection.map((photo, index) => {
+      const isChecked = this.state.selection.indexOf(photo.id) !== -1;
+
+      const actions = (() => {
+        return (
+          <div className="photos-selection__actions">
+            <PhotoPreviewButton photo={photo} />
+            <div className="photos-selection__checkbox">
+              <Checkbox iconStyle={checkboxIconStyle} onCheck={this.handleChange.bind(this, photo)} checked={isChecked} />
+            </div>
+          </div>
+        );
+      })();
+
+      const className = classNames('photos-selection__item', {
+        'is-active': isChecked
+      });
+
+      return (
+        <GridTile key={index} className={className} title={photo.name} actionIcon={actions} >
+          <img src={photo.url} alt={photo.name} />
+        </GridTile>
+      );
+    });
+
+    return (
+      <section className="photos-selection">
+        <header className="photos-selection__header">
+          <h3 className="photos-selection__title">{this.props.title}</h3>
+        </header>
+        <GridList className="photos-selection__grid" cellHeight={200} cols={4}>
+          {thumbs}
+        </GridList>
+      </section>
+    );
+  }
+}
+
+export default PhotosSelection;
