943035b408545f8f467c6ee95afbeb051bbd98dc
Mirosław Ciastek <miroslaw.ciastek@elpassion.com>
2016-07-16 22:23:29 +0200

    Added photos to Results and WheelResults.

--- web/static/js/components/WheelResults.jsx
+++ web/static/js/components/WheelResults.jsx
@@ -3,9 +3,22 @@ import 'css/components/wheel-results.scss';
 import wheelImg from 'images/wheel.svg';
 
 import React from 'react';
+import { GridList, GridTile } from 'material-ui/lib/grid-list';
+
+import PhotoPreviewButton from 'containers/photo/PhotoPreviewButton';
 
 class WheelResults extends React.Component {
   render() {
+    const thumbs = (() => {
+      return this.props.photos.map((photo, index) => {
+        return (
+          <GridTile key={index} title={photo.name} actionIcon={<PhotoPreviewButton photo={photo} />}>
+            <img src={photo.thumb} alt={photo.name} />
+          </GridTile>
+        );
+      });
+    })();
+
     return (
       <section className="wheel-results">
         <div className="wheel-results__left">
@@ -13,7 +26,9 @@ class WheelResults extends React.Component {
         </div>
 
         <div className="wheel-results__right">
-
+          <GridList cellHeight={200} cols={2}>
+            {thumbs}
+          </GridList>
         </div>
       </section>
     );
--- web/static/js/containers/common/Results.jsx
+++ web/static/js/containers/common/Results.jsx
@@ -2,23 +2,36 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { fetchExperiment } from 'actions/experiment';
+import { fetchParticipant } from 'actions/participant';
+
+import LinkButton from 'containers/common/LinkButton';
 
 import WheelResults from 'components/WheelResults';
 
 class Results extends React.Component {
   componentDidMount() {
-    const { experimentId } = this.props.params;
+    const { experimentId, participantId } = this.props.params;
 
     this.props.dispatch(fetchExperiment(experimentId));
+    this.props.dispatch(fetchParticipant(participantId));
   }
 
   render() {
-    const { photos } = this.props.experiment.single;
+    const { experimentId } = this.props.params;
+    const { first_name, last_name } = this.props.participant.single;
+    const { name:experimentName, photos = [] } = this.props.experiment.single;
 
     return (
       <section className="page">
         <header className="page-header">
-          <h1 className="page__title">Results</h1>
+          <div className="page-header__left">
+            <h1 className="page__title">Results for {first_name} {last_name}</h1>
+            <h3 className="page__subtitle">Experiment: "{experimentName}"</h3>
+          </div>
+
+          <div className="page-header__right">
+            <LinkButton label="Back to experiment" primary={true} fullWidth={true} route={`/dashboard/experiments/${experimentId}`} />
+          </div>
         </header>
 
         <section className="page__content">
