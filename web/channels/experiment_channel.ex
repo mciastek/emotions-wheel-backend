7e5fd9cf3e0fe2a73b538a6b8a34b440991a21f7
Mirosław Ciastek <miroslaw.ciastek@elpassion.com>
2016-07-17 16:49:26 +0200

    Updated experiment_channel.

--- web/channels/experiment_channel.ex
+++ web/channels/experiment_channel.ex
@@ -1,16 +1,18 @@
 defmodule EmotionsWheelBackend.ExperimentChannel do
   use EmotionsWheelBackend.Web, :channel
 
-  def join("experiments:lobby", payload, socket) do
-    if authorized?(payload) do
-      {:ok, socket}
-    else
-      {:error, %{reason: "unauthorized"}}
-    end
+  alias EmotionsWheelBackend.{Rate, RateView}
+
+  def join("experiments:results:" <> experiment_id, %{"participant_id" => participant_id}, socket) do
+    rates = saved_rates(experiment_id, participant_id)
+
+    send(self, :after_join)
+
+    {:ok, %{rates: rates}, assign(socket, :rates, rates)}
   end
 
-  def join("experiments:results", _payload, socket) do
-    {:ok, socket}
+  def terminate(_reason, _socket) do
+    :ok
   end
 
   # Channels can be used in a request/response fashion
@@ -26,16 +28,9 @@ defmodule EmotionsWheelBackend.ExperimentChannel do
     {:noreply, socket}
   end
 
-  # This is invoked every time a notification is being broadcast
-  # to the client. The default implementation is just to push it
-  # downstream but one could filter or change the event.
-  def handle_out(event, payload, socket) do
-    push socket, event, payload
-    {:noreply, socket}
-  end
-
-  # Add authorization logic here as required.
-  defp authorized?(_payload) do
-    true
+  defp saved_rates(experiment_id, participant_id) do
+    Rate.by_experiment_participant(experiment_id, participant_id)
+    |> Repo.all
+    |> RateView.render_many
   end
 end
--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule EmotionsWheelBackend.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  channel "experiments:results", EmotionsWheelBackend.ExperimentChannel
+  channel "experiments:*", EmotionsWheelBackend.ExperimentChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
