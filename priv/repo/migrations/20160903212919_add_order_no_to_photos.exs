072a88b3ad15b2c6d2aad414e6b7cfc8fb4a70bf
Mirosław Ciastek <miroslaw.ciastek@elpassion.com>
2016-09-05 00:22:17 +0200

    Update migration.

--- priv/repo/migrations/20160903212919_add_order_no_to_photos.exs
+++ priv/repo/migrations/20160903212919_add_order_no_to_photos.exs
@@ -3,7 +3,7 @@ defmodule EmotionsWheelBackend.Repo.Migrations.AddOrderNoToPhotos do
 
   def change do
     alter table(:experiments_has_photos) do
-      add :order_no, :integer
+      add :order_no, :integer, default: 1
     end
   end
 end
